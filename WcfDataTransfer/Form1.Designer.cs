e04ca39454dfd84e2371e0702e2262f18ac2619f
Ceu <developer.aceu@gmail.com>
2020-09-10 23:00:42 +1000

    implements transforming methods for Client:   - when transferring data over web service, there are some restrictions   - some data such as 2D array can't be exchanged and need to convert to 1D array

--- WcfDataTransfer/Form1.Designer.cs
+++ WcfDataTransfer/Form1.Designer.cs
@@ -28,13 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rowsTextBox = new System.Windows.Forms.TextBox();
+            this.columnsTextBox = new System.Windows.Forms.TextBox();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rows";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Columns";
+            // 
+            // rowsTextBox
+            // 
+            this.rowsTextBox.Location = new System.Drawing.Point(217, 56);
+            this.rowsTextBox.Name = "rowsTextBox";
+            this.rowsTextBox.Size = new System.Drawing.Size(100, 31);
+            this.rowsTextBox.TabIndex = 1;
+            // 
+            // columnsTextBox
+            // 
+            this.columnsTextBox.Location = new System.Drawing.Point(217, 136);
+            this.columnsTextBox.Name = "columnsTextBox";
+            this.columnsTextBox.Size = new System.Drawing.Size(100, 31);
+            this.columnsTextBox.TabIndex = 1;
+            // 
+            // generateButton
+            // 
+            this.generateButton.Location = new System.Drawing.Point(157, 237);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(160, 47);
+            this.generateButton.TabIndex = 2;
+            this.generateButton.Text = "Generate";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1208, 680);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.columnsTextBox);
+            this.Controls.Add(this.rowsTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox rowsTextBox;
+        private System.Windows.Forms.TextBox columnsTextBox;
+        private System.Windows.Forms.Button generateButton;
     }
 }
 
--- WcfDataTransfer/Form1.cs
+++ WcfDataTransfer/Form1.cs
@@ -16,5 +16,53 @@ namespace WcfDataTransfer
         {
             InitializeComponent();
         }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            Int32.TryParse(rowsTextBox.Text, out int rows);
+            Int32.TryParse(columnsTextBox.Text, out int columns);
+            int[,] blocker = new int[rows, columns];
+
+            Randomise(blocker, rows, columns);
+            int[] blocker1D = TransformArray(blocker, rows, columns);
+        }
+
+        private int[] TransformArray(int[,] blocker, int rows, int columns)
+        {
+            int[] blocker1D = new int[rows * columns];
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int column = 0; column < columns; column++)
+                {
+                    blocker1D[row * columns + column] = blocker[rows, column];
+                }
+            }
+
+            return blocker1D;
+        }
+
+        private void Randomise(int[,] data, int rows, int columns)
+        {
+            // this could be used for the yellow part for task allocation
+            Random random = new Random((int)DateTime.Now.Ticks);
+
+            for (int column = 0; column < columns; column++)
+            {
+                int count = 0;
+
+                // 2 is the number of yellow box 
+                while (count < 2)
+                {
+                    int randomRow = random.Next(0, rows - 1);
+
+                    if (data[randomRow, column] == 0)
+                    {
+                        data[randomRow, column] = 1;
+                        count++;
+                    }
+                }
+            }
+        }
     }
 }
