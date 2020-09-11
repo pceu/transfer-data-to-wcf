cd908c19133129f60cd4e739e6eb2cd4843cdb6e
Ceu <developer.aceu@gmail.com>
2020-09-11 15:18:25 +1000

    creates local WCF Service:   - running over the Internet cost and uses just the local one

---
+++ WcfsGenerator/App_Code/IService.cs
@@ -0,0 +1,11 @@
+﻿using System.ServiceModel;
+
+
+[ServiceContract]
+public interface IService
+{
+
+	[OperationContract]
+	string GenerateAllocations(int[,] blocker, int rows, int columns);
+}
+
---
+++ WcfsGenerator/App_Code/Service.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Service : IService
+{
+	public string GenerateAllocations(int[,] blocker, int rows, int columns)
+	{
+		// Return something for now.
+		return (DateTime.Now.Ticks.ToString());
+	}
+}
